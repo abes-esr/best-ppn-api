fcdc0556a189b7c32a0f9624521d2a8cfc489801
maraval <maraval@abes.fr>
2023-07-18 15:48:40 +0200

    CDE-168 : refactor ws bestPpn Ajout paramètre pour ignorer la production dans kafka

--- src/main/java/fr/abes/bestppn/controller/BestPpnController.java
+++ src/main/java/fr/abes/bestppn/controller/BestPpnController.java
@@ -23,7 +23,7 @@ import java.net.URISyntaxException;
 @CrossOrigin(origins = "*")
 @RestController
 @Slf4j
-@RequestMapping("/v1")
+@RequestMapping("/api/v1")
 public class BestPpnController {
     private final BestPpnService service;
 
@@ -31,20 +31,31 @@ public class BestPpnController {
         this.service = service;
     }
     @Operation(
-            summary = "Permet de calculer le best PPN pour une ligne kbart donnée",
+            summary = "Permet de calculer le best PPN pour des paramètres donnés",
             description = "Déroule l'algorithme de calcul du best PPN par appel successif à des webservices",
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Un best PPN a été trouvé", content = {@Content(schema = @Schema())}),
-                    @ApiResponse(responseCode = "400", description = "Erreur dans les paramètres de la requête, ligne Kbart incorrecte", content = {@Content(schema = @Schema())}),
+                    @ApiResponse(responseCode = "200", description = "Le bestPpn identifié", content = {@Content(schema = @Schema())}),
+                    @ApiResponse(responseCode = "400", description = "Erreur dans les paramètres de la requête", content = {@Content(schema = @Schema())}),
                     @ApiResponse(responseCode = "500", description = "Erreur non prise en charge par le système", content = {@Content(schema = @Schema())}),
             }
     )
-    @PostMapping(value = "/bestPpn", consumes = MediaType.APPLICATION_JSON_VALUE, name = "bestPpn")
-    public String bestPpn(@Parameter(description = "a kbart ligne (separated with tabulations)", required = true) @RequestBody KbartProviderDto kbart) throws IOException, BestPpnException, IllegalPpnException {
+    @GetMapping(value = "/bestPpn")
+    public String bestPpn(@RequestParam(name = "provider") String provider, @RequestParam(name = "publication_title", required = false) String publicationTitle,
+                          @RequestParam(name = "publication_type") String publicationType, @RequestParam(name = "online_identifier", required = false) String onlineIdentifier,
+                          @RequestParam(name = "print_identifier") String printIdentifier, @RequestParam(name = "doi", required = false) String doi,
+                          @RequestParam(name = "date_monograph_published_online", required = false) String dateMonographPublishedOnline, @RequestParam(name = "date_monograph_published_print", required = false) String dateMonographPublishedPrint,
+                          @RequestParam(name = "first_author", required = false) String firstAuthor) throws IOException, BestPpnException, IllegalPpnException {
         try {
-            String[] tsvElementsOnOneLine = kbart.getKbart().split("\t");
-            LigneKbartDto ligneKbartDto = constructDto(tsvElementsOnOneLine);
-            return service.getBestPpn(ligneKbartDto, kbart.getProvider());
+            LigneKbartDto ligneKbartDto = new LigneKbartDto();
+            ligneKbartDto.setPublication_type(publicationType);
+            ligneKbartDto.setPublication_title((publicationTitle != null) ? publicationTitle : "");
+            ligneKbartDto.setOnline_identifier((onlineIdentifier != null) ? onlineIdentifier : "");
+            ligneKbartDto.setPrint_identifier((printIdentifier != null) ? printIdentifier : "");
+            ligneKbartDto.setTitle_url((doi != null) ? doi : "");
+            ligneKbartDto.setDate_monograph_published_print((dateMonographPublishedPrint != null) ? dateMonographPublishedPrint : "");
+            ligneKbartDto.setDate_monograph_published_online((dateMonographPublishedOnline != null) ? dateMonographPublishedOnline : "");
+            ligneKbartDto.setFirst_author((firstAuthor != null) ? firstAuthor : "");
+            return service.getBestPpn(ligneKbartDto, provider, false);
         } catch (URISyntaxException e) {
             throw new IllegalArgumentException("Une url dans un champ title_url du kbart n'est pas correcte");
         } catch (IndexOutOfBoundsException e) {
--- src/test/java/fr/abes/bestppn/service/BestPpnServiceTest.java
+++ src/test/java/fr/abes/bestppn/service/BestPpnServiceTest.java
@@ -121,7 +121,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("100000001", result);
@@ -171,7 +171,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("100000001", result);
@@ -228,7 +228,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("100000001", result);
@@ -285,7 +285,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("100000001", result);
@@ -336,7 +336,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Vérification
-        BestPpnException result = Assertions.assertThrows(BestPpnException.class, ()-> bestPpnService.getBestPpn(kbart, provider));
+        BestPpnException result = Assertions.assertThrows(BestPpnException.class, ()-> bestPpnService.getBestPpn(kbart, provider, false));
         Assertions.assertEquals("Les ppn électroniques 100000001, 100000002 ont le même score" , result.getLocalizedMessage());
     }
 
@@ -393,7 +393,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn("300000002")).thenReturn(noticePrint);
 
         //  Vérification
-        BestPpnException result = Assertions.assertThrows(BestPpnException.class, ()-> bestPpnService.getBestPpn(kbart, provider));
+        BestPpnException result = Assertions.assertThrows(BestPpnException.class, ()-> bestPpnService.getBestPpn(kbart, provider, false));
         Assertions.assertEquals("Plusieurs ppn imprimés (100000001, 200000001, 300000002, 300000001) ont été trouvés." , result.getLocalizedMessage());
     }
 
@@ -439,7 +439,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn("300000002")).thenReturn(noticePrint);
 
         //  Appel de la méthode
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("300000001", result);
@@ -485,7 +485,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn("300000002")).thenReturn(noticePrint);
 
         //  Appel de la méthode
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("", result);
@@ -536,7 +536,7 @@ class BestPpnServiceTest {
         Mockito.when(noticeService.getNoticeByPpn(Mockito.anyString())).thenReturn(this.noticeElec);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("100000001", result);
@@ -586,7 +586,7 @@ class BestPpnServiceTest {
         Mockito.when(checkUrlService.checkUrlInNotice(Mockito.anyString(), Mockito.any())).thenReturn(true);
 
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("200000001", result);
@@ -670,7 +670,7 @@ class BestPpnServiceTest {
         Mockito.when(service.callDat2Ppn(kbart.getDate_monograph_published_print(), kbart.getAuthor(), kbart.getPublication_title())).thenReturn(resultDat2PpnWeb);
         Mockito.when(checkUrlService.checkUrlInNotice(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
         //  Appel du service
-        String result = bestPpnService.getBestPpn(kbart, provider);
+        String result = bestPpnService.getBestPpn(kbart, provider, false);
 
         //  Vérification
         Assertions.assertEquals("123456789", result);
@@ -685,7 +685,7 @@ class BestPpnServiceTest {
         ppnElecResultList.put("100000001", 10);
         Set<String> ppnPrintResultList = new HashSet<>();
 
-        String result = bestPpnService.getBestPpnByScore(kbart, provider, ppnElecResultList, ppnPrintResultList);
+        String result = bestPpnService.getBestPpnByScore(kbart, provider, ppnElecResultList, ppnPrintResultList, false);
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals("100000001", result);
     }
@@ -700,7 +700,7 @@ class BestPpnServiceTest {
         ppnElecResultList.put("100000002", 10);
         Set<String> ppnPrintResultList = new HashSet<>();
 
-        String result = bestPpnService.getBestPpnByScore(kbart, provider, ppnElecResultList, ppnPrintResultList);
+        String result = bestPpnService.getBestPpnByScore(kbart, provider, ppnElecResultList, ppnPrintResultList, false);
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals("100000002", result);
     }
