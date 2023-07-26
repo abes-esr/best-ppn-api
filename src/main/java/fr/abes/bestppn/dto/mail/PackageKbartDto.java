1ec47a87e488b405c092363f99acb9f9a83b3fcb
quetin <quetin@abes.fr>
2023-07-26 11:34:37 +0200

    CDE-169 modif properties

--- src/main/java/fr/abes/bestppn/dto/mail/PackageKbartDto.java
+++ src/main/java/fr/abes/bestppn/dto/mail/PackageKbartDto.java
@@ -4,6 +4,7 @@ import fr.abes.bestppn.dto.kafka.LigneKbartDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,10 +12,15 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@Slf4j
 public class PackageKbartDto {
     private List<LigneKbartDto> kbartDtos = new ArrayList<>();
 
     public void addKbartDto(LigneKbartDto ligneKbartDto) {
         this.kbartDtos.add(ligneKbartDto);
+    }
+
+    public void clearKbartDto(){
+        this.kbartDtos.clear();
     }
 }
--- src/main/java/fr/abes/bestppn/kafka/TopicConsumer.java
+++ src/main/java/fr/abes/bestppn/kafka/TopicConsumer.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.abes.bestppn.dto.kafka.LigneKbartDto;
 import fr.abes.bestppn.dto.kafka.PpnKbartProviderDto;
 import fr.abes.bestppn.dto.kafka.PpnWithDestinationDto;
+import fr.abes.bestppn.dto.mail.PackageKbartDto;
 import fr.abes.bestppn.exception.BestPpnException;
 import fr.abes.bestppn.exception.IllegalPpnException;
 import fr.abes.bestppn.exception.IllegalProviderException;
 import fr.abes.bestppn.service.BestPpnService;
+import fr.abes.bestppn.service.EmailService;
 import fr.abes.bestppn.utils.Utils;
 import jakarta.annotation.Resource;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -38,9 +41,13 @@ public class TopicConsumer {
     @Autowired
     private TopicProducer producer;
 
+    @Autowired
+    private EmailService serviceMail;
     private final List<LigneKbartDto> kbartToSend = new ArrayList<>();
 
     private final List<PpnKbartProviderDto> ppnToCreate = new ArrayList<>();
+
+    private final PackageKbartDto mailAttachment = new PackageKbartDto();
 
     boolean isOnError = false;
 
@@ -48,23 +55,28 @@ public class TopicConsumer {
     public void listenKbartFromKafka(ConsumerRecord<String, String> lignesKbart) {
         String filename = "";
         try {
+            for (Header header : lignesKbart.headers().toArray()) {
+                if(header.key().equals("FileName")){
+                    filename = new String(header.value());
+                    break;
+                }
+            }
+            String provider = Utils.extractProvider(filename);
+
             if(lignesKbart.value().equals("OK") ){
                 if( !isOnError ) {
                     producer.sendKbart(kbartToSend, lignesKbart.headers());
                     producer.sendPrintNotice(ppnToCreate, lignesKbart.headers());
+
                 } else {
                     isOnError = false;
                 }
+                serviceMail.sendMailWithAttachment(filename,mailAttachment);
                 kbartToSend.clear();
                 ppnToCreate.clear();
+                mailAttachment.clearKbartDto();
+
             } else {
-                for (Header header : lignesKbart.headers().toArray()) {
-                    if(header.key().equals("FileName")){
-                        filename = new String(header.value());
-                        break;
-                    }
-                }
-                String provider = Utils.extractProvider(filename);
                 LigneKbartDto ligneFromKafka = mapper.readValue(lignesKbart.value(), LigneKbartDto.class);
                 if (ligneFromKafka.isBestPpnEmpty()) {
                     PpnWithDestinationDto ppnWithDestinationDto = service.getBestPpn(ligneFromKafka, provider);
@@ -73,15 +85,13 @@ public class TopicConsumer {
                             ligneFromKafka.setBestPpn(ppnWithDestinationDto.getPpn());
                             kbartToSend.add(ligneFromKafka);
                         }
-                        case PRINT_PPN_SUDOC -> {
-                            ppnToCreate.add(new PpnKbartProviderDto(ppnWithDestinationDto.getPpn(),ligneFromKafka,provider));
-                        }
+                        case PRINT_PPN_SUDOC -> ppnToCreate.add(new PpnKbartProviderDto(ppnWithDestinationDto.getPpn(),ligneFromKafka,provider));
                     }
 
                 } else {
                     kbartToSend.add(ligneFromKafka);
                 }
-
+                mailAttachment.addKbartDto(ligneFromKafka);
             }
         } catch (IllegalProviderException e) {
             isOnError = true;
@@ -89,6 +99,8 @@ public class TopicConsumer {
         } catch (IllegalPpnException | BestPpnException | IOException | URISyntaxException | RestClientException | IllegalArgumentException e) {
             isOnError = true;
             log.error(e.getMessage());
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
