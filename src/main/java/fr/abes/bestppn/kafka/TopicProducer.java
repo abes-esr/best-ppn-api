63923e70aace7473186b95e26a4c0d2217d214ec
pierre-maraval <pierre.maraval@gmail.com>
2024-09-04 10:18:39 +0200

    FIX : Ajout synchronized sur calcul partition

--- src/main/java/fr/abes/bestppn/kafka/TopicProducer.java
+++ src/main/java/fr/abes/bestppn/kafka/TopicProducer.java
@@ -215,14 +215,14 @@ public class TopicProducer {
                 record.key(), record.value(), metadata.topic(), metadata.partition(), metadata.offset(), Stream.of(result.getProducerRecord().headers().toArray()).map(h -> h.key() + ":" + Arrays.toString(h.value())).collect(Collectors.joining(";"))));
     }
 
-    public Integer calculatePartition(int nbPartitions) throws ArithmeticException {
-
+    public Integer calculatePartition(Integer nbPartitions) throws ArithmeticException {
         if (nbPartitions == 0) {
             throw new ArithmeticException("Nombre de threads = 0");
         }
-
-        if (lastThreadUsed.get() >= nbPartitions) {
-            lastThreadUsed.set(0);
+        synchronized (nbPartitions) {
+            if (lastThreadUsed.get() >= nbPartitions) {
+                lastThreadUsed.set(0);
+            }
         }
 
         return lastThreadUsed.getAndIncrement();
